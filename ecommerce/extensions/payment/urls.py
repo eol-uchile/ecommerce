539886ea20ff683b810ddeceeb42184ac3529692
dario617 <spam.dario@gmail.com>
2021-04-09 11:47:06 -0400

    Allow superuser to recover user boletas & tests
    (cherry picked from commit 5e1962bf5e295ed55eb408b1ed9c12adee2ccb17)

--- ecommerce/extensions/payment/urls.py
+++ ecommerce/extensions/payment/urls.py
@@ -42,12 +42,10 @@ urlpatterns = [
     url(r'^paypal/', include((PAYPAL_URLS, 'paypal'))),
     url(r'^sdn/', include((SDN_URLS, 'sdn'))),
     url(r'^stripe/', include((STRIPE_URLS, 'stripe'))),
-    url(r'^webpay/', include((WEBPAY_URLS, 'webpay'))), 
+    url(r'^webpay/', include((WEBPAY_URLS, 'webpay'))),
+    url(r'^boleta/', recover_boleta, name='recover_boleta'),
 ]
 
 for payment_processor_name, urls_module in settings.EXTRA_PAYMENT_PROCESSOR_URLS.items():
     processor_url = url(r'^{}/'.format(payment_processor_name), include((urls_module, payment_processor_name)))
     urlpatterns.append(processor_url)
-
-if hasattr(settings, 'BOLETA_CONFIG') and settings.BOLETA_CONFIG.get('enabled',False):
-    urlpatterns += [url(r'^boleta/', recover_boleta, name='recover_boleta')]
