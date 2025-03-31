1c1acce1a44d7351bf26b04febb8c472f1f057ea
fsologureng <fsologur@uchile.cl>
2025-03-31 17:38:49 -0300

    Polish boleta and webpay (partially cherry picked from commit idcda86780becb6e7a00c617e7a63933928698916)

--- ecommerce/extensions/checkout/views.py
+++ ecommerce/extensions/checkout/views.py
@@ -175,6 +175,19 @@ class ReceiptResponseView(ThankYouView):
             if learner_portal_url:
                 self.add_message_if_enterprise_user(request, enterprise_customer, learner_portal_url)
                 response.context_data['order_dashboard_url'] = learner_portal_url
+            if hasattr(settings, 'BOLETA_CONFIG'):
+                response.context_data['boleta'] = settings.BOLETA_CONFIG['enabled']
+            # Update lines to change titles
+            response.context_data['custom_order_lines'] = []
+            for line in response.context_data['order'].lines.all():
+                custom_line = {}
+                custom_line['title'] = line.title.replace('Seat in','Asiento en'). \
+                    replace('with verified certificate','con certificado verificado'). \
+                    replace('and ID verification','y verificación de identidad')
+                custom_line['quantity'] = line.quantity
+                custom_line['product'] = line.product
+                custom_line['unit_price_incl_tax'] = line.unit_price_incl_tax
+                response.context_data['custom_order_lines'].append(custom_line)
             else:
                 response.context_data['show_receipt_cta_links'] = False
         return response
