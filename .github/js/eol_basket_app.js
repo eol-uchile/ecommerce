c2fdd9a4b7ad97b838f328ceb2cbe64c1ce2fa33
fsologureng <fsologur@uchile.cl>
2025-04-09 16:27:10 -0400

    refactor: changes to js forked to .github  * Changes code to ease to compare with original versions  * TODO: delete this files

--- .github/js/eol_basket_app.js
+++ .github/js/eol_basket_app.js
@@ -1,30 +1,31 @@
-require(["jquery", "eol_pages/basket_page"], function ($, BasketPage) {
-  "use strict";
+require([
+    'jquery',
+    'eol_pages/basket_page'
+  ],
+  function ($,
+        BasketPage) {
+    'use strict';
 
   $(document).ready(function () {
     // TODO: Change to responsive javascript after this site turns into a MFE
     var oldUrl, indexOfFragment, discountString, newUrl;
-    if (
-      window.courseKey !== undefined &&
-      !window.location.href.split("discount_jwt=")[1]
-    ) {
+    if ( window.courseKey !== undefined && !window.location.href.split('discount_jwt=')[1]) {
       $.ajax({
-        url: window.lmsUrlRoot + "/api/discounts/course/" + window.courseKey,
+        url: window.lmsUrlRoot + '/api/discounts/course/' + window.courseKey,
         xhrFields: {
-          withCredentials: true,
-        },
+          withCredentials: true
+        }
       }).done(function (discount) {
         if (discount.discount_applicable) {
           oldUrl = window.location.href;
-          indexOfFragment = oldUrl.indexOf("#");
-          if (oldUrl.includes("?")) {
-            discountString = "&discount_jwt=" + discount.jwt;
+          indexOfFragment = oldUrl.indexOf('#');
+          if (oldUrl.includes('?')) {
+            discountString = '&discount_jwt=' + discount.jwt;
           } else {
-            discountString = "?discount_jwt=" + discount.jwt;
+            discountString = '?discount_jwt=' + discount.jwt;
           }
           if (indexOfFragment > -1) {
-            newUrl =
-              oldUrl.substring(0, indexOfFragment) +
+            newUrl = oldUrl.substring(0, indexOfFragment) +
               discountString +
               oldUrl.substring(indexOfFragment);
           } else {
@@ -38,4 +39,5 @@ require(["jquery", "eol_pages/basket_page"], function ($, BasketPage) {
 
     BasketPage.onReady();
   });
-});
+}
+);
