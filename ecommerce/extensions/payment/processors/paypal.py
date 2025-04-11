b8469d645c71cfe818ab910d6b8c9446349e3aaa
fsologureng <fsologur@uchile.cl>
2025-04-11 15:31:34 -0400

    fix: allows set OSCAR_DEFAULT_CURRENCY to CLP & paypal payment
    Currency conversion CLPtoUSD asumption didn't set up basket currency to USD, which fails in Paypal payment API.

--- ecommerce/extensions/payment/processors/paypal.py
+++ ecommerce/extensions/payment/processors/paypal.py
@@ -171,7 +171,7 @@ class Paypal(EolBillingMixin, BasePaymentProcessor):
             'transactions': [{
                 'amount': {
                     'total': str(self.parseCLPtoUSD(basket.total_incl_tax)),
-                    'currency': basket.currency,
+                    'currency': 'USD',
                 },
                 # Paypal allows us to send additional transaction related data in 'description' & 'custom' field
                 # Free form field, max length 127 characters
@@ -188,7 +188,7 @@ class Paypal(EolBillingMixin, BasePaymentProcessor):
                             # PayPal requires that the sum of all the item prices (where price = price * quantity)
                             # equals to the total amount set in amount['total'].
                             'price': str(self.parseCLPtoUSD(line.line_price_incl_tax_incl_discounts) / line.quantity),
-                            'currency': line.stockrecord.price_currency,
+                            'currency': 'USD',
                         }
                         for line in basket.all_lines()
                     ],
